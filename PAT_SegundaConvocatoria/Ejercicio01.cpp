0fee5f62b8ff7410886d29255f0e16b61bccd76a
Guille3002 <70027905+Guille3002@users.noreply.github.com>
2023-11-30 08:38:33 -0600

    Update Ejercicio01.cpp

--- PAT_SegundaConvocatoria/Ejercicio01.cpp
+++ PAT_SegundaConvocatoria/Ejercicio01.cpp
@@ -1,6 +1,23 @@
 #include "Ejercicio01.h"
 
-vector<string> Ejercicio01::generateParenthesis(int n)
-{
-    return vector<string>();
+vector<string> Ejercicio01::generateParenthesis(int n) {
+    vector<string> result;
+    generateParenthesisHelper("", 0, 0, n, result);
+    return result;
 }
+
+void Ejercicio01::generateParenthesisHelper(string current, int open, int close, int n, vector<string>& result) {
+    if (current.size() == 2 * n) {
+        result.push_back(current);
+        return;
+    }
+
+    if (open < n) {
+        generateParenthesisHelper(current + '(', open + 1, close, n, result);
+    }
+
+    if (close < open) {
+        generateParenthesisHelper(current + ')', open, close + 1, n, result);
+    }
+}
+
